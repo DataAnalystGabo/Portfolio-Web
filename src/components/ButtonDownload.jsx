04198c5524ecd1d7ca19cb1008f05cc2ada2923a
Gonzalo Ramirez <gonzalogabrielramirez@outlook.com.ar>
2024-09-01 22:06:58 -0300

    Se añadió el cv en formato pdf en su versión 7.0 al boton de descarga

--- src/components/ButtonDownload.jsx
+++ src/components/ButtonDownload.jsx
@@ -1,34 +1,37 @@
 import styled from "styled-components";
+import cvV7 from "@assets/cvV7.pdf";
 
 export function ButtonDownload({ text = "This is button" }) {
-  return (
-    <>
-      <Button>{text}</Button>
-    </>
-  );
+    return (
+        <>
+            <a href={cvV7} download="cv-gonzalo-ramirez.pdf">
+                <Button>{text}</Button>
+            </a>
+        </>
+    );
 }
 
 const Button = styled.button`
-  width: max-content;
-  padding: 0.8rem 2rem;
-  border: 2px solid var(--primary-color);
-  color: var(--primary-color);
-  background-color: var(--background-sidebar);
-  box-shadow: 0.25rem 0.25rem var(--secondary-text);
-  font-weight: var(--font-weight-medium);
-  transition: all 0.2s;
-  cursor: pointer;
-  box-sizing: border-box;
-  &:hover {
-    color: var(--primary-color);
+    width: max-content;
+    padding: 0.8rem 2rem;
     border: 2px solid var(--primary-color);
+    color: var(--primary-color);
     background-color: var(--background-sidebar);
     box-shadow: 0.25rem 0.25rem var(--secondary-text);
-  }
-  &:active {
-    transform: translate(0.25rem, 0.25rem);
-    box-shadow: none;
-  }
-  @media (max-width: 950px) {
-  }
+    font-weight: var(--font-weight-medium);
+    transition: all 0.2s;
+    cursor: pointer;
+    box-sizing: border-box;
+    &:hover {
+        color: var(--primary-color);
+        border: 2px solid var(--primary-color);
+        background-color: var(--background-sidebar);
+        box-shadow: 0.25rem 0.25rem var(--secondary-text);
+    }
+    &:active {
+        transform: translate(0.25rem, 0.25rem);
+        box-shadow: none;
+    }
+    @media (max-width: 950px) {
+    }
 `;
